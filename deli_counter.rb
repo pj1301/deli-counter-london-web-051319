f02834b4f61069a40b17467c02ca74f8c79556fc
James Hayward <james.hayward6@icloud.com>
2019-04-23 02:39:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def line (line)
   if line.length == 0
   puts "The line is currently empty."
 else
-  puts "The line is currently: #{numline.join()}"
+  puts "The line is currently: #{num_line.join()}"
 end
 end
 
