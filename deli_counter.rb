72f47b0ad7ae804ef2b322042f481dea0d378228
James Hayward <james.hayward6@icloud.com>
2019-04-23 02:53:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def take_a_number(line, name)
   puts "Welcome, #{name}. You are number #{line.length} in line."
 end
 
-def now_serving()
+def now_serving(line)
   if line.length == 0
     puts "There is nobody waiting to be served!"
   else
